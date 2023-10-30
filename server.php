c3a155d36127d3a69c1db0eacbaebc3324987665
Alethepata <alessia.patane@gmail.com>
2023-10-30 16:50:19 +0100

    [Add] $_POST

--- server.php
+++ server.php
@@ -4,4 +4,16 @@ $string = file_get_contents('todo-list.json');
 
 $list = json_decode($string);
 
+if(isset($_POST['todo'])){
+
+    $newItem = $_POST['todo'];
+    $list[] = $newItem;
+
+    file_put_contents('todo-list.json', json_encode($list));
+
+};
+
+header('Content-Type: application/json');
+
 echo json_encode($list);
+
